be8f4f8e58613d1e2dd89fcf364624646d66ed1c
yungkit9 <yungkithui@gmail.com>
2019-02-20 12:18:35 +0100

    naar second page is gelukt

--- app/src/main/java/com/project/hadd/ConnectActivity.java
+++ app/src/main/java/com/project/hadd/ConnectActivity.java
@@ -6,27 +6,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class ConnectActivity extends AppCompatActivity {
     private Button button;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_connect);
 
-        button = findViewById(R.id.Connectionbutton);
+        button = findViewById(R.id.backBtn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openConnect();
+                backMain();
             }
         });
-
     }
-    public void openConnect(){
-        Intent intent = new Intent(this, ConnectActivity.class);
-        startActivity(intent);
-    }
+        public void backMain(){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
 }
