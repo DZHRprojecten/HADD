35d643a33eb2a869c9b60807f9bd035fd6812460
DZoetmulder <dannyzoetmulder@gmail.com>
2019-03-04 12:24:04 +0100

    Update met colors en dingen verwijderd

---
+++ app/src/main/java/com/project/hadd/Themechange.java
@@ -0,0 +1,14 @@
+package com.project.hadd;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Themechange extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_themechange);
+    }
+
+}
