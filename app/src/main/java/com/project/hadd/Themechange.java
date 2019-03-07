fc82f6779a9952cb3c5d0ca3642fbbd4d5da7eb1
DZoetmulder <dannyzoetmulder@gmail.com>
2019-03-07 13:41:52 +0100

    Colors pagina marges verbeterd

--- app/src/main/java/com/project/hadd/Themechange.java
+++ app/src/main/java/com/project/hadd/Themechange.java
@@ -14,7 +14,21 @@ public class Themechange extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_themechange, container, false);
-    }
+        View view = inflater.inflate(R.layout.activity_themechange, container, false);
+        view.findViewById(R.id.bdonkerblauw);
+        view.findViewById(R.id.blichtblauw);
+        view.findViewById(R.id.bdonkergroen);
+        view.findViewById(R.id.blichtgroen);
+        view.findViewById(R.id.bgeel);
+        view.findViewById(R.id.broze);
+        view.findViewById(R.id.bzwart);
+        view.findViewById(R.id.bgrijs);
+        view.findViewById(R.id.brood);
+        view.findViewById(R.id.bpaars);
+        view.findViewById(R.id.boranje);
+        view.findViewById(R.id.bbruin);
+        view.findViewById(R.id.bbasisthema);
+        return view;
 
+    }
 }
