191bc47e539e109ed684d2e6d8b53759f83b5411
yungkit9 <yungkithui@gmail.com>
2019-03-07 11:58:29 +0100

    navigatie veranderd

--- app/src/main/java/com/project/hadd/Themechange.java
+++ app/src/main/java/com/project/hadd/Themechange.java
@@ -2,13 +2,19 @@ package com.project.hadd;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class Themechange extends Activity {
+public class Themechange extends Fragment {
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_themechange);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_themechange, container, false);
     }
 
 }
